e29cf507969a8dde1a826a3307ccd6e046b8a5cf
Ganesh1609 <ganeshnamdevgaikwad@gmail.com>
2026-02-28 20:40:06 +0530

    Premitive Data Types

---
+++ src/main/java/org/example/primitveDataTypes/PrimitiveDataTypes.java
@@ -0,0 +1,26 @@
+package org.example.primitveDataTypes;
+
+public class PrimitiveDataTypes {
+    public static void main(String [] args){
+
+        System.out.println("Byte Range MIN "+ Byte.MIN_VALUE);
+        System.out.println("Byte Range MAX "+ Byte.MAX_VALUE);
+        byte age1=-30;
+        System.out.println("***********************************");
+        System.out.println();
+        System.out.println("Short Range MIN "+ Short.MIN_VALUE);
+        System.out.println("Short Range MAX "+ Short.MAX_VALUE);
+        short age2=40;
+        System.out.println("***********************************");
+        System.out.println();
+        System.out.println("Integer Range MIN "+ Integer.MIN_VALUE);
+        System.out.println("Integer Range MAX "+ Integer.MAX_VALUE);
+        int age =20;
+        System.out.println("***********************************");
+        System.out.println();
+        System.out.println("Long Range MIN "+ Long.MIN_VALUE);
+        System.out.println("Long Range MAX "+ Long.MAX_VALUE);
+        long age3=50;
+
+    }
+}
